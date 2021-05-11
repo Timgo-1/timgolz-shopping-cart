4993d47b027da448432c65faa5872a8dba4b57b2
timgo-1 <timochars@gmail.com>
2021-05-11 13:55:42 +0300

    Timgolz App#3

--- src/index.js
+++ src/index.js
@@ -11,4 +11,33 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+const path = require("path");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+app.use(morgan("tiny")); // logging framework
+
+// Serve our api message
+app.get("/api/message", async (req, res, next) => {
+  try {
+    res.status(201).json({ message: "HELLOOOOO FROM EXPRESS" });
+  } catch (err) {
+    next(err);
+  }
+});
+
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  app.use(express.static("build"));
+
+  // Express will serve up the front-end index.html file if it doesn't recognize the route
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve("build", "index.html"))
+  );
+}
+
+// Choose the port and start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
 
