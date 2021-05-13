a8e51ef87bd1994a3208f1d647169c4b7004dd55
timgo-1 <timochars@gmail.com>
2021-05-13 18:06:48 +0300

    Timgolz App#3

--- server.js
+++ server.js
@@ -95,5 +95,5 @@ app.post("/api/orders", async (req, res) => {
     res.send(order);
   });
   
-const port = process.env.PORT || 5000;
+const port = process.env.PORT ||  "https://timgolz-shopping-cart.herokuapp.com/";
 app.listen(port, () => console.log("serve at https://timgolz-shopping-cart.herokuapp.com/"));
