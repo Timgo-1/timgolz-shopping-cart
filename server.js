fb281e88294c43447c454fb0f9bcf9acdd0a5439
timgo-1 <timochars@gmail.com>
2021-05-23 18:37:04 +0300

    #We-Move

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
+import dotenv from 'dotenv';
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const shortid = require("shortid");
 
 const app = express();
+dotenv.config();
 app.use(bodyParser.json());
 
 app.use("/", express.static(__dirname + "/build"));
 app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
 
+// app.get('/', (req, res) => {
+//   res.send('Hello to Timgolz-Shopping-Cart API');
+// });
+
 mongoose.connect(
-    process.env.MONGODB_URL || "mongodb://localhost/timgolz-shopping-cart-db",
+    process.env.CONNECTION_URL || "http://localhost:5000/",
+
      {
         useNewUrlParser: true,
         useCreateIndex: true,
